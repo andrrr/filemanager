edf6c9253ece251b2ad45bff85b915080f6b9039
andrrr <andrey.lubinov@gmail.com>
2010-04-09 12:05:06 +0300

    Russian lang file

---
+++ lang/lang.ru.php
@@ -0,0 +1,51 @@
+<?php
+
+	$about = array(
+		'name' => 'Русский',
+		'author' => array(
+			'name' => 'Andrey Lubinov',
+			'email' => 'andrey.lubinov@gmail.com',
+			'website' => false
+		),
+		'release-date' => '2010-04-09'
+	);
+
+
+	/*
+	 * EXTENSION: File Manager
+	 * Localisation strings
+	 */
+
+	$dictionary = array(
+
+		'File Manager' =>
+		'Менеджер файлов',
+
+		'System' =>
+		'Менеджер файлов',
+
+		'' =>
+		'',
+
+		'' =>
+		'',
+
+		'' =>
+		'',
+
+		'' =>
+		'',
+
+		'' =>
+		'',
+
+		'' =>
+		'',
+
+		'' =>
+		'',
+
+		'' =>
+		'',
+
+	);
